d1ca5ab589cb6f5812d0449b3c16e7fcb7f53042
Ginyola <gintokyyola@gmail.com>
2019-09-04 20:43:53 +0300

    Добавлен тест для третьего задания

--- 0/src/MainClassTest.java
+++ 0/src/MainClassTest.java
@@ -14,7 +14,22 @@ public class MainClassTest {
     @Test
     public void testGetClassNumber(){
         MainClass mainClass = new MainClass();
-        Assert.assertTrue("Метод getClassNumber вернул число меньше или равное 45",
+        Assert.assertTrue("Метод getClassNumber вернул число меньше или равное 45\n" +
+                        "Результат выполенеия: " + mainClass.getClassNumber() + "\n",
                 mainClass.getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString(){
+        String[] searchedSubStringArray = new String[] {
+                "Hello", "hello"
+        };
+        MainClass mainClass = new MainClass();
+        for(String subStr : searchedSubStringArray) {
+            Assert.assertTrue("Метод getClassString вернул строку не содержащую нужную подстроку\n" +
+                            "Полученная строка: " + mainClass.getClassString() + "\n" +
+                            "Поиск строки: " + subStr,
+                    mainClass.getClassString().contains(subStr));
+        }
     }
 }
