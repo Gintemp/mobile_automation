1922e1cfb9957edbcf480bddf39053ac3a9375d2
Gintemp <gintokyyola@gmail.com>
2019-09-25 22:09:30 +0300

    Задание Ex2: Создание метода

--- 2/JavaAppiumAutomation/src/FirstClassTest.java
+++ 2/JavaAppiumAutomation/src/FirstClassTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -21,7 +26,7 @@ public class FirstClassTest {
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","D:\\git\\mobile_automation\\1\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        capabilities.setCapability("app","D:\\git\\mobile_automation\\2\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
@@ -33,8 +38,60 @@ public class FirstClassTest {
     }
 
     @Test
-    public void firstTest()
+    public void findSearchText()
     {
-        System.out.println("Hello, Appium!");
+        String searchedString = "Search…";
+        String searchInputInactiveId = "org.wikipedia:id/search_container";
+        String searchInputInProcessId = "org.wikipedia:id/search_src_text";
+
+        waitForElementAndClick(
+                By.id(searchInputInactiveId),
+                "Element " + searchInputInactiveId + " not found",
+                5
+        );
+        WebElement element = waitForElementPresent(By.id(searchInputInProcessId), searchInputInactiveId);
+        Assert.assertEquals(
+                "Expected default text in search field is not equal " + searchedString,
+                searchedString,
+                element.getAttribute("text"));
+    }
+
+    private WebElement waitForElementPresent(By by, String searchInputInactiveId)
+    {
+        return waitForElementPresent(by, "Element " + searchInputInactiveId + " not found", 5);
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeout)
+    {
+        WebDriverWait driverWait = new WebDriverWait(driver, timeout);
+        driverWait.withMessage(errorMessage + "\n");
+        return driverWait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeout)
+    {
+        WebElement element = waitForElementPresent(by, errorMessage, timeout);
+        element.click();
+
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String errorMessage, long timeout)
+    {
+        WebElement element = waitForElementPresent(by, errorMessage, timeout);
+        element.sendKeys(value);
+
+        return element;
+    }
+
+    private boolean waiForElementNotPresent(By by, String errorMessage, long timeout)
+    {
+        WebDriverWait driverWait = new WebDriverWait(driver, timeout);
+        driverWait.withMessage(errorMessage);
+        return driverWait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
     }
 }
