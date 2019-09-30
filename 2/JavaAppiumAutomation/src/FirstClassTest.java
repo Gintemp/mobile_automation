dabd2fd62910ed234eaf792e87e15964b642c68b
Gintemp <gintokyyola@gmail.com>
2019-09-30 20:16:39 +0300

    Добавлен тест Ex3: Тест: отмена поиска

--- 2/JavaAppiumAutomation/src/FirstClassTest.java
+++ 2/JavaAppiumAutomation/src/FirstClassTest.java
@@ -11,35 +11,33 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstClassTest {
 
     private AppiumDriver driver;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","D:\\git\\mobile_automation\\2\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "D:\\git\\mobile_automation\\2\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void findSearchText()
-    {
+    public void findSearchText() {
         String searchedString = "Search…";
         String searchInputInactiveId = "org.wikipedia:id/search_container";
         String searchInputInProcessId = "org.wikipedia:id/search_src_text";
@@ -49,20 +47,43 @@ public class FirstClassTest {
                 "Element " + searchInputInactiveId + " not found",
                 5
         );
-        WebElement element = waitForElementPresent(By.id(searchInputInProcessId), searchInputInactiveId);
+        WebElement element = waitForElementPresent(By.id(searchInputInProcessId));
         Assert.assertEquals(
                 "Expected default text in search field is not equal " + searchedString,
                 searchedString,
                 element.getAttribute("text"));
     }
 
-    private WebElement waitForElementPresent(By by, String searchInputInactiveId)
-    {
-        return waitForElementPresent(by, "Element " + searchInputInactiveId + " not found", 5);
+    @Test
+    public void searchTextAndClearResult() {
+        String searchedString = "Java";
+        String searchInputInactiveId = "org.wikipedia:id/search_container";
+        String searchInputInProcessId = "org.wikipedia:id/search_src_text";
+        String searchResultId = "org.wikipedia:id/page_list_item_container";
+        String cancelSearchId = "org.wikipedia:id/search_close_btn";
+
+        waitForElementAndClick(By.id(searchInputInactiveId));
+        waitForElementAndSendKeys(By.id(searchInputInProcessId), searchedString);
+        waitForElementPresent(By.id(searchResultId));
+
+        List<WebElement> articlesList = driver.findElements(By.id(searchResultId));
+        Assert.assertNotEquals(
+                "Amount of articles equal 0. Searched string: " + searchedString, articlesList.size(), 0);
+
+        waitForElementAndClick(By.id(cancelSearchId));
+        waitForElementNotPresent(By.id(searchResultId));
+
+        List<WebElement> emptyArticlesList = driver.findElements(By.id(searchResultId));
+        Assert.assertEquals(
+                "Amount of articles not equal 0.", emptyArticlesList.size(), 0);
+
     }
 
-    private WebElement waitForElementPresent(By by, String errorMessage, long timeout)
-    {
+    private WebElement waitForElementPresent(By by) {
+        return waitForElementPresent(by, "Element " + by.toString() + " not found", 5);
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeout) {
         WebDriverWait driverWait = new WebDriverWait(driver, timeout);
         driverWait.withMessage(errorMessage + "\n");
         return driverWait.until(
@@ -70,28 +91,39 @@ public class FirstClassTest {
         );
     }
 
-    private WebElement waitForElementAndClick(By by, String errorMessage, long timeout)
-    {
+    private WebElement waitForElementAndClick(By by) {
+        return waitForElementAndClick(by, "Element " + by.toString() + " not found", 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeout) {
         WebElement element = waitForElementPresent(by, errorMessage, timeout);
         element.click();
 
         return element;
     }
 
-    private WebElement waitForElementAndSendKeys(By by, String value, String errorMessage, long timeout)
-    {
+    private WebElement waitForElementAndSendKeys(By by, String value) {
+        return waitForElementAndSendKeys(by, value, "Error\n", 10);
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String errorMessage, long timeout) {
         WebElement element = waitForElementPresent(by, errorMessage, timeout);
         element.sendKeys(value);
 
         return element;
     }
 
-    private boolean waiForElementNotPresent(By by, String errorMessage, long timeout)
-    {
+    private boolean waitForElementNotPresent(By by) {
+        return waitForElementNotPresent(by, "Error: element " + by.toString() + "has been found\n", 5);
+    }
+
+    private boolean waitForElementNotPresent(By by, String errorMessage, long timeout) {
         WebDriverWait driverWait = new WebDriverWait(driver, timeout);
         driverWait.withMessage(errorMessage);
         return driverWait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
     }
+
+
 }
